b14a35c150ca02a526be5c33476a044cfab80e61
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2019-10-17 14:37:13 +0200

    samples: echo-client.c PSK support
    PSK support is added to echo-client.c.
    Surrounded by #ifdef ... #endif in order to
    not affect original sample.
    
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- samples/net/sockets/echo_client/src/ca_certificate.h
+++ samples/net/sockets/echo_client/src/ca_certificate.h
@@ -8,6 +8,7 @@
 #define __CA_CERTIFICATE_H__
 
 #define CA_CERTIFICATE_TAG 1
+#define PSK_TAG 2
 
 #define TLS_PEER_HOSTNAME "localhost"
 
@@ -16,5 +17,10 @@
 static const unsigned char ca_certificate[] = {
 #include "echo-apps-cert.der.inc"
 };
+
+#if defined(CONFIG_MBEDTLS_KEY_EXCHANGE_PSK_ENABLED)
+static const unsigned char client_psk[] = {0x01, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+static const char client_psk_id[] = "Client_identity";
+#endif
 
 #endif /* __CA_CERTIFICATE_H__ */
